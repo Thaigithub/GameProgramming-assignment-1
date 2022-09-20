c655d9ddbbeb2dc62c8fc865f1ae4a8ab53ef695
Thagithub <1tranquocthai@gmail.com>
2022-09-20 13:18:56 +0700

    Update

--- game_page.py
+++ game_page.py
@@ -1,4 +1,4 @@
-
+import pygame
 framerate = 60
 
 def time_to_string(time):
@@ -7,17 +7,18 @@ def time_to_string(time):
     second = str(time%60) if (time%60)>=10 else "0"+str(time%60)
     return minute+":"+second
 
-def game_page(pg, time):
+# def game_page(pygame, time):
+def game_page(time):
     #Load image
-    background = pg.image.load("image/Background.jpg")
-    zombie = pg.image.load("image/Zombie.png")
-    zombie = pg.transform.scale(zombie,(60,60))
-    mallet1 = pg.image.load("image/Mallet1.png")
-    mallet1 = pg.transform.scale(mallet1,(100,100))
-    mallet2 = pg.image.load("image/Mallet2.png")
-    mallet2 = pg.transform.scale(mallet2,(100,100))
+    background = pygame.image.load("image/Background.jpygame")
+    zombie = pygame.image.load("image/Zombie.png")
+    zombie = pygame.transform.scale(zombie,(60,60))
+    mallet1 = pygame.image.load("image/Mallet1.png")
+    mallet1 = pygame.transform.scale(mallet1,(100,100))
+    mallet2 = pygame.image.load("image/Mallet2.png")
+    mallet2 = pygame.transform.scale(mallet2,(100,100))
     #Set up system
-    surface = pg.display.set_mode((background.get_width(), background.get_height()))
+    surface = pygame.display.set_mode((background.get_width(), background.get_height()))
     timer_event = {}
     #Game logic
     zombie_matrix = []
@@ -30,29 +31,29 @@ def game_page(pg, time):
             zombie_rect_list[i].append(zombie.get_rect(center = (93+105*j,83+140*i)))
     #Set up display utility object
     display_object = {}
-    display_object["countdown"] = [pg.font.SysFont(None, 50)]
+    display_object["countdown"] = [pygame.font.SysFont(None, 50)]
     display_object["countdown"].append(display_object["countdown"][0].render(time_to_string(time), True, (255, 255, 255)))
-    timer_event["countdown"] = pg.USEREVENT+len(timer_event)+1
-    pg.time.set_timer(timer_event["countdown"], 1000)
+    timer_event["countdown"] = pygame.USEREVENT+len(timer_event)+1
+    pygame.time.set_timer(timer_event["countdown"], 1000)
     #Start game
     while True:
         #Input and process
         mouseclick = False
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 return False
             if event.type == timer_event["countdown"]:
                 time -= 1
                 display_object["countdown"][1] = display_object["countdown"][0].render(time_to_string(time), True, (255, 255, 255))
                 if time == 0:
-                    pg.time.set_timer(timer_event["countdown"],0)
+                    pygame.time.set_timer(timer_event["countdown"],0)
                     return True
-            if event.type == pg.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 mouseclick = True
                 check = False
                 for i in range(5):
                     for j in range(9):
-                        if zombie_rect_list[i][j].collidepoint(pg.mouse.get_pos()) and zombie_matrix[i][j]:
+                        if zombie_rect_list[i][j].collidepoint(pygame.mouse.get_pos()) and zombie_matrix[i][j]:
                             check = True
                             break
                 if check: print("Hit")
@@ -64,7 +65,7 @@ def game_page(pg, time):
                 if zombie_matrix[i][j]:
                     surface.blit(zombie,zombie_rect_list[i][j])
         print(mouseclick)
-        if mouseclick: surface.blit(mallet2,mallet2.get_rect(center = (pg.mouse.get_pos()[0]+40,pg.mouse.get_pos()[1]-20)))
-        else: surface.blit(mallet1,mallet1.get_rect(center = (pg.mouse.get_pos()[0]+40,pg.mouse.get_pos()[1]-20)))
+        if mouseclick: surface.blit(mallet2,mallet2.get_rect(center = (pygame.mouse.get_pos()[0]+40,pygame.mouse.get_pos()[1]-20)))
+        else: surface.blit(mallet1,mallet1.get_rect(center = (pygame.mouse.get_pos()[0]+40,pygame.mouse.get_pos()[1]-20)))
         surface.blit(display_object["countdown"][1], display_object["countdown"][1].get_rect(center = (900, 750)))
-        pg.display.flip()
+        pygame.display.flip()
